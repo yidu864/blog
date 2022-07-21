f79ffb05fcb99bc5b348992609dae452ca10fe51
yidu864 <943070101@qq.com>
2022-07-21 23:27:46 +0800

    chore(config): giscus config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -122,7 +122,7 @@ export default defineUserConfig<GungnirThemeOptions>({
         repoId: 'R_kgDOHBjyCQ',
         category: 'Announcements',
         categoryId: 'DIC_kwDOHBjyCc4COKZm',
-        darkTheme: 'http://yidu864.github.io/style/giscus-dark.css',
+        darkTheme: 'https://yidu864.github.io/style/giscus-dark.css',
       },
       mdPlus: {
         all: true,
